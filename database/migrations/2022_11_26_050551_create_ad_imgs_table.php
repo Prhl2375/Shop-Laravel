c1675eab21e87b7e74837691e70fe1cbb04779e4
Prhl2375 <dahippie@pechka.localdomain>
2022-11-27 06:18:55 +0200

    laravel migrate, db seeder, docker-compose

---
+++ database/migrations/2022_11_26_050551_create_ad_imgs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ad_imgs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img');
+            $table->boolean('display');
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ad_imgs');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,84 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        for ($k = 1; $k<=13; $k++){
+            DB::table('products')->insert([
+                'name' => 'p-' . $k,
+                'price' => rand(20, 2000),
+                'display' => 1,
+                'picture' => 'p-' . $k . '.png',
+                'tags' => '',
+                'discount_number' => rand(0, 75)
+            ]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($k = 1; $k<=3; $k++){
+            DB::table('banners')->insert([
+                'name' => 'bnr-' . $k,
+                'display' => 1,
+                'img' => 'bnr-' . $k . '.jpg',
+                'link' => '#'
+            ]);
+        }
+        for ($k = 4; $k<=5; $k++){
+            DB::table('banners')->insert([
+                'name' => 'bnr-' . $k,
+                'display' => 1,
+                'img' => 'bnr-' . $k . '.png',
+                'link' => '#'
+            ]);
+        }
+        for ($k = 1; $k<=3; $k++){
+            DB::table('ad_imgs')->insert([
+                'name' => 'ad-' . $k,
+                'display' => 1,
+                'img' => 'ad-' . $k . '.jpg',
+                'link' => '#'
+            ]);
+        }
+        DB::table('currencies')->insert([
+            'name' => 'USD',
+            'prefix' => '$',
+            'postfix' => '',
+            'rate' => 1,
+            'updated_at' => Carbon::yesterday()->subYear(),
+            'main' => '1'
+        ]);
+        DB::table('currencies')->insert([
+            'name' => 'UAH',
+            'prefix' => '',
+            'postfix' => '₴',
+            'rate' => 1,
+            'updated_at' => Carbon::yesterday()->subYear(),
+            'main' => '0'
+        ]);
+        DB::table('currencies')->insert([
+            'name' => 'EUR',
+            'prefix' => '€',
+            'postfix' => '',
+            'rate' => 1,
+            'updated_at' => Carbon::yesterday()->subYear(),
+            'main' => '0'
+        ]);
+        DB::table('currencies')->insert([
+            'name' => 'JPY',
+            'prefix' => '¥',
+            'postfix' => '',
+            'rate' => 1,
+            'updated_at' => Carbon::yesterday()->subYear(),
+            'main' => '0'
+        ]);
+        for ($k = 1; $k<=3; $k++){
+            DB::table('single_product_imgs')->insert([
+                'product_id' => 1,
+                'img' => 's_p-1_' . $k . '.png',
+            ]);
+        }
+        for ($k = 2; $k<=13; $k++){
+            DB::table('single_product_imgs')->insert([
+                'product_id' => $k,
+                'img' => 's_p-' . $k . '_1' . '.png',
+            ]);
+        }
     }
 }
